af2542f775a214026f4c36074aff79e99e9939c0
Simon <62201840+simonros123@users.noreply.github.com>
2022-11-07 12:20:09 +0100

    Main directory fuel prices e5

--- source/dashboardMain.py
+++ source/dashboardMain.py
@@ -2,12 +2,12 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 import datetime
+from dateutil import tz
 
 # Anaconda prompt:
-# cd C:\Users\lcami\Desktop\Master\Semester 1\BI\Projekt
 # streamlit run dashboardDemo.py
+#..\venv\Scripts\streamlit.exe run dashboardMain.py
 
-# abort: strg c
 
 # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
 st.set_page_config(page_title="Dashboard",
@@ -15,33 +15,38 @@ st.set_page_config(page_title="Dashboard",
                    layout="wide"
                    )
 
-df = pd.read_csv('mvp_station_prices_2019-10_omv_donzdorf.csv', sep=';')
+df = pd.read_csv('data/mvp_station_prices_2019-10_omv_donzdorf.csv', sep=',')
 
 d5 = st.date_input("date range without default", [datetime.date(2019, 10, 1), datetime.date(2019, 10, 31)])
 st.write(d5)
 
+EUR = tz.gettz('Europe / Berlin')
+from_datetime = datetime.datetime(d5[0].year, d5[0].month, d5[0].day, 0, 0, 0, tzinfo= EUR).isoformat(sep=" ",timespec= "seconds")
+to_datetime = datetime.datetime(d5[1].year, d5[1].month, d5[1].day, 23, 59, 59, tzinfo= EUR).isoformat(sep= " ",timespec= "seconds")
+print(from_datetime)
+
 df_selection = df.query(
-    "date between @d5[0] and @d5[1]"
+    "date >= @from_datetime and date <= @to_datetime"
 )
 
 st.dataframe(df_selection)
 
 # -----Mainpage------
-st.title(":bar_chart: fuelprice Dashboard")
+st.title(":line_chart: fuelprice Dashboard")
 st.markdown("##")
 
 fuel_price_e5 = float(df["e5"].count())
 
-left_col = st.columns(1)
-with left_col:
-    st.subheader("Fuel price")
+#left_col = st.columns(1)
+#with left_col:
+#    st.subheader("Fuel price")
 
 
 st.markdown("----")
 fig_fuelprice = px.line(
     df_selection,
-    x="Tag",
-    y="Preis",
+    x="date",
+    y="e5",
     title="<b> Fuel Price </b>",
     template="plotly_dark",
 )
