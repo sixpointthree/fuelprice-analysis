66eeca2ac246f907e99c56392424c0c58c0172fe
Steffen Lang <steffenlang1994@gmail.com>
2022-11-10 17:38:57 +0100

    database-model: add db model for Prices and Stations

---
+++ source/data_access/db_model.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, Float
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+# create table prices with timestamp, price, station_id
+class Prices(Base):
+    __tablename__ = 'prices'
+    id = Column(Integer, primary_key=True)
+    timestamp = Column(Integer)
+    station_id = Column(Integer, ForeignKey('stations.id'))
+    price_diesel = Column(Float)
+    price_e5 = Column(Float)
+    price_e10 = Column(Float)
+
+# create table stations with id, name, brand, street, place, lat, lng
+class Stations(Base):
+    __tablename__ = 'stations'
+    id = Column(Integer, primary_key=True)
+    uuid = Column(String)
+    name = Column(String)
+    brand = Column(String)
+    street = Column(String)
+    place = Column(String)
+    lat = Column(Integer)
+    lng = Column(Integer)
