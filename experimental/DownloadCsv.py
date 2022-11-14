c774a876da452fad598ac512c8f91f5185d2677e
Lisa <l.caminati@web.de>
2022-11-14 17:39:03 +0100

    Experimental: download csv

---
+++ experimental/DownloadCsv.py
@@ -0,0 +1,23 @@
+import csv
+import requests
+import datetime
+
+# CSV_URL = 'https://dev.azure.com/tankerkoenig/362e70d1-bafa-4cf7-a346-1f3613304973/_apis/git/repositories/0d6e7286-91e4-402c-af56-fa75be1f223d/items?path=/prices/2022/01/2022-01-01-prices.csv&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=octetStream&api-version=5.0&download=true'
+
+date = '2022-01-03'
+monthOfYear = '2022/01/'
+
+CSV_URL = f'https://dev.azure.com/tankerkoenig/362e70d1-bafa-4cf7-a346-1f3613304973/_apis/git/repositories/0d6e7286-91e4-402c-af56-fa75be1f223d/items?path=/prices/' + monthOfYear + date + '-prices.csv&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=octetStream&api-version=5.0&download=true'
+
+with requests.Session() as s:
+    download = s.get(CSV_URL)
+
+    decoded_content = download.content.decode('utf-8')
+
+    cr = csv.reader(decoded_content.splitlines(), delimiter=',')
+    my_list = list(cr)
+    i = 0
+    for row in my_list:
+        if i < 5:
+            print(row)
+            i += 1
