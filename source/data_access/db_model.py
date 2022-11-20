9a6ef9cad2fece0e68916b34cc70a2e73a9caf29
Steffen Lang <steffenlang1994@gmail.com>
2022-11-20 14:54:30 +0100

    database-model: add create and remove function. path is now 'data/fuelprices.db'

--- source/data_access/db_model.py
+++ source/data_access/db_model.py
@@ -2,8 +2,9 @@ from sqlalchemy import Column, Integer, String, ForeignKey, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import os
 
-DB_NAME = 'fuelprices.db'
+DB_NAME = 'data/fuelprices.db'
 PRICES_TABLE_NAME = 'prices'
 STATIONS_TABLE_NAME = 'stations'
 
@@ -32,4 +33,21 @@ class Stations(Base):
     lat = Column(Integer)
     lon = Column(Integer)
 
-Base.metadata.create_all(engine)
+
+#create db path recursively
+def create_db_path():
+    path = os.path.dirname(DB_NAME)
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+def remove_db():
+    if os.path.exists(DB_NAME):
+        os.remove(DB_NAME)
+        print(f'Removed db {DB_NAME}')
+
+def create_db():
+    create_db_path()
+    Base.metadata.create_all(engine)
+
+
+create_db()
