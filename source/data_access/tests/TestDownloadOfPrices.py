3374df899d6e3e765735345c01ae19e455d1535b
Steffen Lang <steffenlang1994@gmail.com>
2022-11-20 14:54:55 +0100

    database-model: Adapt changes

--- source/data_access/tests/TestDownloadOfPrices.py
+++ source/data_access/tests/TestDownloadOfPrices.py
@@ -4,6 +4,7 @@ import dateutil.tz
 
 from source.data_access.functions import get_prices, get_prices_by_date_tz
 from datetime import datetime
+from source.data_access.db_model import remove_db, create_db
 
 TIMESTAMP_2020_01_01_BEGIN = datetime(2020, 1, 1, 0, 0, 0)
 TIMESTAMP_2020_01_01_END = datetime(2020, 1, 1, 23, 59, 59)
@@ -20,23 +21,30 @@ KNOWN_RECORD_CNT_MVP_STAT_2020_01_01_2020_01_03 = 135
 # test get_data_from_db
 class TestGetDataFromDb(unittest.TestCase):
     def test_one_day_get_data_from_db(self):
+        create_db()
         self.assertTrue(len(get_prices(from_datetime=int(TIMESTAMP_2020_01_01_BEGIN.timestamp()),
                                        to_datetime=int(TIMESTAMP_2020_01_01_END.timestamp())))
                         == KNOWN_RECORD_CNT_2020_01_01)
+        remove_db()
 
     def test_multiple_days_get_data_from_db(self):
+        create_db()
         self.assertTrue(len(get_prices(from_datetime=int(TIMESTAMP_2020_01_01_BEGIN.timestamp()),
                                        to_datetime=int(TIMESTAMP_2020_01_03_END.timestamp())))
                         == KNOWN_RECORD_CNT_2020_01_01_2020_01_03)
+        remove_db()
 
     def test_get_data_from_db_mvp_stations(self):
+        create_db()
         self.assertTrue(len(get_prices(from_datetime=int(TIMESTAMP_2020_01_01_BEGIN.timestamp()),
                                        to_datetime=int(TIMESTAMP_2020_01_03_END.timestamp()),
                                        station_uuids=[UUID_OMV_DONZDORF, UUID_ARAL_GOEPPINGEN,
                                                             UUID_SHELL_GOEPPINGEN]))
                         == KNOWN_RECORD_CNT_MVP_STAT_2020_01_01_2020_01_03)
+        remove_db()
 
     def test_get_prices_high_level_func(self):
+        create_db()
         prices = get_prices_by_date_tz(from_date=TIMESTAMP_2020_01_01_BEGIN,
                                        to_date=TIMESTAMP_2020_01_03_END,
                                        station_uuids=[UUID_OMV_DONZDORF, UUID_ARAL_GOEPPINGEN,
@@ -45,6 +53,7 @@ class TestGetDataFromDb(unittest.TestCase):
         self.assertTrue(len(prices)
                         == KNOWN_RECORD_CNT_MVP_STAT_2020_01_01_2020_01_03)
         self.assertTrue(prices['timestamp'].dt.tz == dateutil.tz.gettz("Europe/Berlin"))
+        remove_db()
 
 
 if __name__ == '__main__':
--- source/data_access/tests/TestGetMVPStationInfo.py
+++ source/data_access/tests/TestGetMVPStationInfo.py
@@ -1,9 +1,11 @@
 import unittest
 from source.data_access.functions import get_station_info_uuid, check_stations_csv
+from source.data_access.db_model import remove_db
 
 UUID_OMV_DONZDORF = "16f07bfd-0bde-4126-a393-ea8a7d053283"
 UUID_ARAL_GOEPPINGEN = "77c4cc3c-ae11-43c4-85cc-5c147409b46f"
 UUID_SHELL_GOEPPINGEN = "c13f60cb-7e1c-40a8-b05a-157fd571b3fa"
+
 
 class TestGetMVPStationInfo(unittest.TestCase):
     def test_get_station_info_uuid(self):
@@ -14,6 +16,8 @@ class TestGetMVPStationInfo(unittest.TestCase):
         self.assertEqual("aral", info.brand)
         info = get_station_info_uuid(UUID_SHELL_GOEPPINGEN)
         self.assertEqual("shell", info.brand)
+        remove_db()
+
 
 if __name__ == '__main__':
     unittest.main()
