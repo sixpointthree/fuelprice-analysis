0bc7573dd435171ced68c4cd5ba7be262de333ea
Steffen Lang <steffenlang1994@gmail.com>
2022-12-09 16:05:40 +0100

    Unittests: Add create_db() and remove_db() for reproducable results

--- source/location/tests/find_stations_from_db_by_location_test.py
+++ source/location/tests/find_stations_from_db_by_location_test.py
@@ -1,6 +1,7 @@
 import unittest
 from source.location.find_stations import find_stations_from_db_by_location
 from source.data_access.functions import check_stations_csv
+from source.data_access.db_model import remove_db, create_db
 
 QUERY_OMV_DONZDORF = "omv donzdorf"
 QUERY_SHELL_GÖPPINGEN = "shell göppingen"
@@ -12,6 +13,7 @@ QUERY_BERLIN = "berlin"
 
 class TestFindStations(unittest.TestCase):
     def test_find_stations_by_location(self):
+        create_db()
         check_stations_csv()
         stations = find_stations_from_db_by_location(QUERY_OMV_DONZDORF, radius_km=0.1)
         self.assertEqual(1, len(stations))
@@ -25,26 +27,35 @@ class TestFindStations(unittest.TestCase):
         self.assertEqual(1, len(stations))
         print(stations)
         self.assertEqual("shell", stations.iloc[0].brand)
+        remove_db()
 
     def test_find_stations_geislingen(self):
+        create_db()
         check_stations_csv()
         stations = find_stations_from_db_by_location(QUERY_GEISLINGEN, radius_km=4)
         self.assertGreater(len(stations), 0)
+        remove_db()
 
     def test_find_by_plz(self):
+        create_db()
         check_stations_csv()
         stations_plz = find_stations_from_db_by_location(QUERY_PLZ_GEISLINGEN, radius_km=4)
         stations_city = find_stations_from_db_by_location(QUERY_GEISLINGEN, radius_km=4)
         self.assertEqual(len(stations_plz), len(stations_city))
+        remove_db()
 
     def test_find_many_stations(self):
+        create_db()
         check_stations_csv()
         stations = find_stations_from_db_by_location(QUERY_BERLIN, radius_km=100)
         self.assertGreater(len(stations), 0)
+        remove_db()
 
     def test_no_query(self):
+        create_db()
         stations = find_stations_from_db_by_location("", radius_km=1)
         self.assertEqual(len(stations), 0)
+        remove_db()
 
 
 
